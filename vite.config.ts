4833278699333a903dd7fae32d54366f0bc0be48
dlinning <dlinning@users.noreply.github.com>
2026-03-03 00:09:16 -0600

    Last ugh for the night

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/weather-eink/",
+	// base: "/weather-eink/",
 	plugins: [vue(), vueJsx(), vueDevTools()],
 	resolve: {
 		alias: {
